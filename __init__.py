9b4a2792e00e3fd5a7f381dbfd9d68e15ac5bd30
SethTales <34636186+SethTales@users.noreply.github.com>
2018-03-19 21:05:27 -0700

    Barebones application skeleton

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .SolarCalc import app
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='SolarCalc',
+    packages=['SolarCalc'],
+    include_package_data=True,
+    install_requires=['flask',
+    ],
+)
